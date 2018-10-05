c3092c19805d5a4cd3488b3d8d0c573d1476eb5f
dreamzz2000 <dreamzz2000@163.com>
2018-10-05 23:02:04 +0800

    1005

---
+++ HC/main.js
@@ -0,0 +1,7 @@
+//alert("helloworld!");
+var
+	content = document.getElementsByTagName('contentpage'),
+	abc = document.createElement('div');
+abc.id = "abc";
+abc.innerText = "123456435431238712378612368176923";
+content.appendChild(abc);
