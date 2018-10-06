b53ab27a27ee178b606413b648a6b1d451eff364
dreamzz2000 <dreamzz2000@163.com>
2018-10-06 21:22:36 +0800

    2122

--- HC/main.js
+++ HC/main.js
@@ -1,7 +1,9 @@
 //alert("helloworld!");
 var
-	content = document.getElementsByTagName('contentpage'),
+	content = document.getElementsByClassName('contentpage'),
 	abc = document.createElement('div');
 abc.id = "abc";
 abc.innerText = "123456435431238712378612368176923";
-content.appendChild(abc);
+console.log("666");
+test1 = document.getElementsByClassName('text');
+console.log(test1);
