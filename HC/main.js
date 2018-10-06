6cdb45740d61f2f8006717c184250d3130efa04f
dreamzz2000 <dreamzz2000@163.com>
2018-10-06 23:31:10 +0800

    1006

--- HC/main.js
+++ HC/main.js
@@ -1,11 +1,34 @@
 //alert("helloworld!");
-var g123 = document.querySelector(".bo");
-g123.innerText = "123";
 
-var test1 = document.createElement('div');
-test1.className = 'content';
-test1.innerText = "TEXT!!!!!";
+var page = document.querySelector(".contentpage");
 
-var asd = document.querySelector(".contentpage");
+var x = 0;
 
-asd.appendChild(test1);
+var arr = [];
+
+for(x=0; x<=2; x++)
+{
+    arr[x] = document.createElement('div');
+    arr[x].className = 'content';
+    page.appendChild(arr[x]);
+}
+
+var 
+    con1 = document.createElement('div'),
+    pic1 = document.createElement('div'),
+    bo1 = document.createElement('div'),
+    text1 = document.createElement('div'),
+    h11 = document.createElement('h1');
+
+con1.className = 'content';
+pic1.className = 'pic';
+bo1.className = 'bo';
+text1.className = 'text';
+
+
+
+page.appendChild(con1);
+con1.appendChild(pic1);
+con1.appendChild(bo1);
+con1.appendChild(h11);
+con1.appendChild(text1);
